59b13f46ad0213dbfb9400c88859b986c0c75c45
Justin Wood <woody@apple.com>
2017-05-08 23:37:32 -0700

    Cleaning up

--- mqtt-alexa-bridge.js
+++ mqtt-alexa-bridge.js
@@ -1,24 +1,26 @@
 // Requirements
-mqtt = require('mqtt')
+const mqtt = require('mqtt')
+const express = require('express')
+const url = require('url')
 
-logging = require('./homeautomation-js-lib/logging.js')
-mqtt_helpers = require('./homeautomation-js-lib/mqtt_helpers.js')
+const logging = require('./homeautomation-js-lib/logging.js')
+require('./homeautomation-js-lib/mqtt_helpers.js')
 
 
 // Config
-host = process.env.MQTT_HOST
+const host = process.env.MQTT_HOST
 
 // Set up modules
-logging.set_enabled(fatruelse)
+logging.set_enabled(true)
 
 // Setup MQTT
-client = mqtt.connect(host)
+var client = mqtt.connect(host)
 
 // MQTT Observation
 
 client.on('connect', () => {
     logging.log('Reconnecting...\n')
-    client.subscribe("#")
+    client.subscribe('#')
 })
 
 client.on('disconnect', () => {
@@ -27,5 +29,26 @@ client.on('disconnect', () => {
 })
 
 client.on('message', (topic, message) => {
-    logging.log(" " + topic + ":" + message)
+    logging.log(' ' + topic + ':' + message)
+})
+
+
+// Web front end
+var app = express()
+
+app.get('/smarthome', function(req, res) {
+    var url_info = url.parse(req.url, true)
+    var topic = url_info.pathname.slice(6)
+    var value = url_info.query.value
+    var accessToken = req.access_token
+    logging.log('req: ' + JSON.stringify(req))
+
+    const accessURL = 'https://api.amazon.com/user/profile?access_token=' + accessToken
+
+
+    res.send('topic: ' + topic + ' value: ' + value)
+})
+
+app.listen(3000, function() {
+    logging.log('IFTTT listener started on port 3000')
 })
