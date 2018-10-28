bc3c6ec102da59a02c795f515286d08b4046e750
terafin <jwood@me.com>
2018-10-28 00:53:17 -0700

    Updating

--- mqtt-alexa-bridge.js
+++ mqtt-alexa-bridge.js
@@ -5,8 +5,8 @@ const config = require('homeautomation-js-lib/config_loading.js')
 const logging = require('homeautomation-js-lib/logging.js')
 const _ = require('lodash')
 const health = require('homeautomation-js-lib/health.js')
-const FauxMo = require('fauxmojs')
 const ip = require('ip')
+const wemore = require('wemore')
 
 
 
@@ -24,36 +24,69 @@ var client = mqtt.setupClient()
 
 config.load_path(configPath)
 
-var devicesConfig = []
+
+var processAction = function(shouldRetain, value, topic, callback) {
+	if (!_.isNil(value) && !_.isNil(topic)) {
+		client.publish(topic, '' + value, {retain: shouldRetain})
+		logging.info('alexa action', {
+			'action': 'alexa-request',
+			'topic': topic,
+			'value': value,
+		})
+	}
+
+	if (!_.isNil(callback)) {
+		return callback() 
+	}
+
+	return true
+}
+
+const handleDeviceAction = function(action, deviceConfig) {
+	const message = action == 'on' ? '1' : '0'
+	const topic = deviceConfig.topic
+	const actions = deviceConfig.actions
+	var options = deviceConfig.options
+
+	if (_.isNil(options)) { 
+		options = {}
+	}
+
+	if (!_.isNil(actions)) {
+		async.eachOf(actions, processAction.bind(undefined, options.retain))
+	}
+
+	processAction(options.retain, message, topic)
+}
+
+
 
 config.on('config-loaded', () => {
 	logging.debug('  Alexa config loaded')
-	devicesConfig = {}
- 
-	// {
-	// 	ipAddress: '192.168.1.230',
-	// 	devices: [{
-	// 		name: 'office light',
-	// 		port: 11000,
-	// 		handler: (action) => {
-	// 			console.log('office light action:', action)
-	// 		}
-	// 	},
-	// 	{
-	// 		name: 'office fan',
-	// 		port: 11001,
-	// 		handler: (action) => {
-	// 			console.log('office fan action:', action)
-	// 		}
-	// 	}]
-	// }
-
-	const ipAddress = ip.address() // my ip address
-
-	devicesConfig.ipAddress = ipAddress
-	devicesConfig.devices = []
 
 	config.deviceIterator(function(deviceName, deviceConfig) {
+
+		var thisDevice = wemore.Emulate({friendlyName: deviceConfig.name, port: deviceConfig.port})
+
+		thisDevice.on('listening', function() {
+			logging.info(deviceConfig.name + ' listening on', this.port)
+		})
+
+		thisDevice.on('state', function(binaryState, self, sender) {
+			logging.info(deviceConfig.name + ' set to=', binaryState)
+		})
+
+		// also, 'on' and 'off' events corresponding to binary state
+		thisDevice.on('on', function(self, sender) {
+			logging.info(deviceConfig.name + ' turned on')
+			handleDeviceAction('on', deviceConfig)
+		})
+
+		thisDevice.on('off', function(self, sender) {
+			logging.info(deviceConfig.name + ' turned off')
+			handleDeviceAction('off', deviceConfig)
+		})
+
 		var deviceInfo = {
 			name: deviceConfig.name,
 			port: deviceConfig.port,
@@ -63,40 +96,8 @@ config.on('config-loaded', () => {
 				logging.info(deviceName + ' topic:' +  deviceConfig.topic)
 			}
 		}
-		devicesConfig.devices.push(deviceInfo)
+
 		logging.info('  found device info', deviceInfo)
-	})
-
-	let fauxMo = new FauxMo(devicesConfig)
-	
-	logging.info('Started WeMo emulation with: ' + JSON.stringify(devicesConfig))
-	
-})
-
-
-
-var wemore = require('wemore')
-
-// note that each device needs a separate port:
-var tv = wemore.Emulate({friendlyName: 'TV', port: 9001}) // choose a port
-var stereo = wemore.Emulate({friendlyName: 'Stereo'}) // automatically assigned
-
-stereo.on('listening', function() {
-	// if you want it, you can get it:
-	console.log('Stereo listening on', this.port)
-})
-
-tv.on('state', function(binaryState, self, sender) {
-	console.log('TV set to=', binaryState)
-	tv.close() // stop advertising the device
-})
-
-// also, 'on' and 'off' events corresponding to binary state
-stereo.on('on', function(self, sender) {
-	console.log('Stereo turned on')
-})
-
-stereo.on('off', function(self, sender) {
-	console.log('Stereo turned off')
+	})	
 })
 
